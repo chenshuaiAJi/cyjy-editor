fd17bce8f9967ab48bf1dbb9cb00cf0d82dc10b9
chenshuai <943388027@qq.com>
2025-05-23 17:31:04 +0800

    type(module): 调整u标签实现自定义元素添加下划线

--- packages/basic-modules/src/modules/text-style/helper.ts
+++ packages/basic-modules/src/modules/text-style/helper.ts
@@ -4,7 +4,9 @@
  */
 
 import { DomEditor, IDomEditor } from '@wangeditor-next/core'
-import { Editor, Node } from 'slate'
+import {
+  Editor, Node, Text, Transforms,
+} from 'slate'
 
 export function isMenuDisabled(editor: IDomEditor, _mark?: string): boolean {
   if (editor.selection == null) { return true }
@@ -12,6 +14,14 @@ export function isMenuDisabled(editor: IDomEditor, _mark?: string): boolean {
   const [match] = Editor.nodes(editor, {
     match: n => {
       const type = DomEditor.getNodeType(n)
+
+      if (
+        editor
+          .getFragment()
+          .every((el: any) => el.children.every((node: any) => node.type === 'PINYIN' || node.text === ''))
+      ) {
+        return false
+      }
 
       if (type === 'pre') { return true } // 代码块
       if (Editor.isVoid(editor, n)) { return true } // void node
@@ -38,4 +48,85 @@ export function removeMarks(editor: IDomEditor, textNode: Node) {
     // 其他属性，全部清除
     Editor.removeMark(editor, key)
   })
+}
+
+// 移除元素节点的指定mark
+export function removeNodeMarks(editor: IDomEditor, key: string) {
+  if (key === 'text') {
+    return
+  }
+  const nodeMarks = Editor.nodes(editor, {
+    match: n => n[key],
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [_node, path] of nodeMarks) {
+    Transforms.unsetNodes(editor, key, { at: path })
+  }
+}
+
+// 判断选中内容是否是文本
+export function isSelectedAllText(editor: IDomEditor) {
+  // 获取选中的所有节点
+  const selectedNodes = Array.from(
+    Editor.nodes(editor, {
+      match: () => true,
+      universal: true,
+      mode: 'lowest',
+    }),
+  )
+  // 检查是否有节点被选中
+
+  if (selectedNodes.length === 0) {
+    return false
+  }
+  // 检查选中的节点是否都是文本节点
+  const isAllText = selectedNodes.every(([node]) => {
+    if (Text.isText(node)) {
+      return true
+    }
+    return false
+  })
+
+  return isAllText
+}
+
+/**
+ * 为自定义元素设置指定mark
+ * @param editor
+ * @param props
+ * @param marksNeedToRemove
+ * @returns
+ */
+export function setNodeMarks(
+  editor: IDomEditor,
+  props: any,
+  marksNeedToRemove: string[],
+  nodeType: string[],
+) {
+  const key = Object.keys(props)[0]
+  // 检查是否有选区
+
+  if (editor.selection == null) {
+    return
+  }
+  const markedNodesArray = Array.from(
+    Editor.nodes(editor, {
+      match: n => n[key],
+    }),
+  )
+  const targetNodes = Editor.nodes(editor, {
+    match: n => nodeType.includes(DomEditor.getNodeType(n)),
+  })
+
+  if (markedNodesArray.length) {
+    removeNodeMarks(editor, key)
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const [_node, path] of targetNodes) {
+      Transforms.setNodes(editor, props, { at: path })
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      marksNeedToRemove.forEach(key => removeNodeMarks(editor, key))
+    }
+  }
 }
--- packages/basic-modules/src/modules/text-style/menu/UnderlineMenu.ts
+++ packages/basic-modules/src/modules/text-style/menu/UnderlineMenu.ts
@@ -3,19 +3,50 @@
  * @author wangfupeng
  */
 
-import { t } from '@wangeditor-next/core'
+import { IDomEditor, t } from '@wangeditor-next/core'
 
 import { UNDER_LINE_SVG } from '../../../constants/icon-svg'
+import {
+  isSelectedAllText,
+  setNodeMarks,
+} from '../helper'
 import BaseMenu from './BaseMenu'
 
 class UnderlineMenu extends BaseMenu {
   readonly mark = 'underline'
+
+  readonly marksNeedToRemove = ['wavy', 'stress'] // 增加 mark 的同时，需要移除哪些 mark （互斥，不能共存的）
 
   readonly title = t('textStyle.underline')
 
   readonly iconSvg = UNDER_LINE_SVG
 
   readonly hotkey = 'mod+u'
+
+  /**
+   * 执行命令
+   * @param editor editor
+   * @param value
+   */
+  exec(editor: IDomEditor, value: string | boolean) {
+    console.error('undeerline1', editor)
+
+    const { mark, marksNeedToRemove } = this
+
+    if (!isSelectedAllText(editor)) {
+      setNodeMarks(editor, { [mark]: true }, marksNeedToRemove, ['PINYIN'])
+    } else if (value) {
+      // 已，则取消
+      editor.removeMark(mark)
+    } else {
+      // 没有，则执行
+      editor.addMark(mark, true)
+      // 移除互斥、不能共存的 marks
+      if (marksNeedToRemove) {
+        marksNeedToRemove.forEach(m => editor.removeMark(m))
+      }
+    }
+  }
 }
 
 export default UnderlineMenu
